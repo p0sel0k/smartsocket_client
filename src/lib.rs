3e5fff50b51104ba66527ddd0480446e94f8bea2
p0sel0k <antonpoleshchuk56@gmail.com>
2022-07-14 10:19:28 +0300

    message: fix errors

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ pub enum SocketClientError {
     BadHandshake(String),
     #[error("Io error")]
     Io(#[from] io::Error),
+    #[error("BadEncoding")]
+    BadEncoding,
 }
 
 pub struct SocketClient {
@@ -30,23 +32,39 @@ impl SocketClient {
 
     fn try_handshake(mut stream: TcpStream) -> SocketClientResult<Self> {
         stream.write_all(b"smart")?;
-        let mut buf = [0; 6];
+        let mut buf = [0; 4];
         stream.read_exact(&mut buf)?;
-        if &buf != b"socket" {
+        if &buf != b"home" {
             let msg = format!("recieved string is: {:?}", buf);
             return Err(SocketClientError::BadHandshake(msg));
         }
         Ok(Self { stream })
     }
 
-    pub fn get_power(&mut self) -> SocketClientResult<u32> {
+    pub fn get_power(&mut self) -> SocketClientResult<String> {
         let send_buf = "get power".as_bytes();
-        let mut recieve_buf = [0; 4];
-        let len = send_buf.len() as u32;
-        self.stream.write_all(len.to_be_bytes().as_slice())?;
+        let mut recieve_buf_len = [0; 4];
+        let send_len = send_buf.len() as u32;
+        self.stream.write_all(send_len.to_be_bytes().as_slice())?;
         self.stream.write_all(send_buf)?;
+        self.stream.read_exact(&mut recieve_buf_len)?;
+        let recieve_len = u32::from_be_bytes(recieve_buf_len);
+        let mut recieve_buf = vec![0; recieve_len as _];
+        println!("Response len is: {}", recieve_len);
         self.stream.read_exact(&mut recieve_buf)?;
-        let power = u32::from_be_bytes(recieve_buf);
-        Ok(power)
+        String::from_utf8(recieve_buf).map_err(|_| SocketClientError::BadEncoding)
+    }
+
+    pub fn switch(&mut self) -> SocketClientResult<String> {
+        let send_buf = "switch it".as_bytes();
+        let mut recieve_buf_len = [0; 4];
+        let send_len = send_buf.len() as u32;
+        self.stream.write_all(send_len.to_be_bytes().as_slice())?;
+        self.stream.write_all(send_buf)?;
+        self.stream.read_exact(&mut recieve_buf_len)?;
+        let recieve_len = u32::from_be_bytes(recieve_buf_len);
+        let mut recieve_buf = vec![0; recieve_len as _];
+        self.stream.read_exact(&mut recieve_buf)?;
+        String::from_utf8(recieve_buf).map_err(|_| SocketClientError::BadEncoding)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ use anyhow::Result;
 use smartsocket_client::SocketClient;
 
 fn main() -> Result<()> {
-    let addr = "localhost:8095";
+    let addr = "127.0.0.1:8095";
     let mut client = SocketClient::connect_to_socket(addr)?;
     println!("Client succcessfully connected to SmartSocket");
-    println!(
-        "Socket on {} power has power: {} ",
-        addr,
-        client.get_power()?
-    );
+    println!("Socket on {} has power: {} ", addr, client.get_power()?);
     Ok(())
 }
